5375dff488099241b5b0a9cfc8534ff91539bbcc
Tab Fitts <tfitts@spryservers.net>
2021-05-24 11:47:44 -0700

    Update appinfos and composer.json

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,4 +1,6 @@
 <?php
+
+declare(strict_types=1);
 /**
  * Create your routes in here. The name is the lowercase name of the controller
  * without the controller part, the stuff after the hash is the method.
@@ -7,9 +9,22 @@
  * The controller class has to be registered in the application.php file since
  * it's instantiated in there
  */
-return [
-    'routes' => [
-	   ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-	   ['name' => 'page#do_echo', 'url' => '/echo', 'verb' => 'POST'],
-    ]
-];
+ return [
+ 	'routes' => [
+ 		[
+ 			'name' => 'settings#startVerification',
+ 			'url' => '/settings/enable',
+ 			'verb' => 'POST'
+ 		],
+ 		[
+ 			'name' => 'settings#revokeVerification',
+ 			'url' => '/settings/disable',
+ 			'verb' => 'DELETE'
+ 		],
+ 		[
+ 			'name' => 'settings#finishVerification',
+ 			'url' => '/settings/validate',
+ 			'verb' => 'POST'
+ 		]
+ 	]
+ ];
